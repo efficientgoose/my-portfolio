82bb2842826e54b910446d9ce4b66a11cc6fe98e
ajinkode <ajinkode@gmail.com>
2024-11-02 19:31:08 +0530

    Finished About page and started with blog page

---
+++ src/slices/ContentIndex/index.tsx
@@ -0,0 +1,40 @@
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import ContentList from "./ContentList";
+
+/**
+ * Props for `ContentIndex`.
+ */
+export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
+
+/**
+ * Component for "ContentIndex" Slices.
+ */
+const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+
+  
+
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Heading className="mb-8" size="xl">
+        {slice.primary.heading}
+      </Heading>
+
+      {isFilled.richText(slice.primary.description) && (
+        <div className="prose prose-xl prose-invert mb-10">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+      )}
+
+      <ContentList />
+    </Bounded>
+  );
+};
+
+export default ContentIndex;
+
